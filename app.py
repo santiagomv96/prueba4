f6dffa89d4d776cc0ca34c00c066152a33d3d4b5
Santiago Marulanda <santiagomv.96@gmail.com>
2023-04-11 23:07:51 -0500

    streamlit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def leer_dato(image):
     img_array = 1 - (img_array / 255)
     img_array = img_array.reshape(1, 784)
 
-    return dato
+    return img_array
 
 # Características básicas de la página
 st.set_page_config(page_icon="📊", page_title="Detección de numeros con mnist", layout="wide")
@@ -30,33 +30,25 @@ with c30:
     if uploaded_file is not None:        
         image = Image.open(uploaded_file) 
         #mostrar img cargada
-        st.image(image, caption='Uploaded Image', use_column_width=True)
-
-        file_container = st.expander("Verifique el archivo .pkl que acaba de subir")
-
-        info_box_wait = st.info(f"""Realizando la clasificación...""")
+        # file_container = st.expander("Verifique el archivo .pkl que acaba de subir")
+        # info_box_wait = st.info(f"""Realizando la clasificación...""")
 
         # Acá viene la predicción con el modelo
-        dato = leer_dato(uploaded_file)
-        
+        dato = leer_dato(image)  
+        # image = image.resize((28, 28), resample=Image.BILINEAR)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
         model_cargado = joblib.load("modelo_entrenado.pkl")
         
         # rl = Autoencoder()
         # autoencoder = cargar_modelo_preentrenado('autoencoder.pth')
         # prediccion = predecir(autoencoder, dato, UMBRAL)
-        prediccion=model_cargado.predict(dato.reshape(1, -1))
+        prediccion=model_cargado.predict(dato)
         # categoria = obtener_categoria(prediccion)
 
         # Y mostrar el resultado
-        info_box_result = st.info(f"""
-        	El dato analizado corresponde a un sujeto: {prediccion[0]}
-        	""")
+        info_box_result = st.info(f"""El dato analizado corresponde a un sujeto: {prediccion[0]}""")
 
-    else:
-        st.info(
-            f"""
-                👆 Debe cargar primero un dato con extensión .pkl
-                """
-        )
+    # else:
+    #     st.info(f"""👆 Debe cargar primero un dato con extensión .pkl""")
 
         st.stop()
